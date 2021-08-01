9819bf5d8d88d1f58d5544ace145c535158bfe4d
rarpda <ricardoarpa@hotmail.com>
2021-08-01 11:20:24 +0100

    Created header using grid.

---
+++ common/Form.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Form({...name}) {
+    return (
+    <div className={name}>
+        <modal id="addForm" hidden>
+            
+        </modal>
+    </div>
+  )
+}
